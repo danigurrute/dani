c82aff47d9ae9e01dbcfaaf8ca377194daa3fdbf
danigurrute <danabolaos11@misena.edu.co>
2022-05-27 16:54:56 -0500

    onclick (para todo)

--- app/src/main/java/com/example/splashscreenapp/MainActivity.java
+++ app/src/main/java/com/example/splashscreenapp/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.splashscreenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.recolector:
+                Intent intent=new Intent( this, Usuariorecolector.class );
+                startActivity(intent);
+                break;
+
+        }
     }
 }
